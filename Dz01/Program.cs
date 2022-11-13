69ced7c1c8a00862d311bc60d74ea26c5fd6b7c9
NEVA <neva.vka@yandex.ru>
2022-11-13 21:33:15 +0300

    Задача 64,66

--- Dz01/Program.cs
+++ Dz01/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿// Задача 64.
+
+void NatNum(int m, int n)
+{
+    if (m <= n)
+    {
+    Console.Write(m + " ");
+    NatNum(m+1,n);
+    }
+}
+NatNum(4,9);
+
+// Задача 66.
+
+int SumNat(int m, int n)
+{
+    if (m <= n)
+    {
+    return m + SumNat(m+1,n);
+    }
+    return 0;
+}
+Console.WriteLine(SumNat(2,6));
